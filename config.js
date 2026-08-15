46ba2d4532d0f84fb62bfe7bf38e275957dcaa23
Dylan1-5 <dilanbricenolopez15@gmail.com>
2026-08-15 03:27:19 -0600

    Change command prefix and subprefix characters

--- config.js
+++ config.js
@@ -12,8 +12,8 @@ global.owner = [
 
 global.dev = 'Dy' // Tu nombre o alias
 global.botName = 'Bot_2' // Nombre de tu bot
-global.prefix = ['/'] // Prefijo por defecto
-global.subprefix = ['.']
+global.prefix = ['×'] // Prefijo por defecto
+global.subprefix = ['π']
 global.banner = 'https://files.catbox.moe/u2viza.jpg' // Banner del menú 
 
 // FUNCIÓN DE RECARGA AUTOMÁTICA
